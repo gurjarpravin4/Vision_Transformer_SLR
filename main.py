923a91f98f96ab50ed8a49db9c525caa931b6ec0
gurjarpravin4 <gurjarpravin4@gmail.com>
2023-10-27 09:21:52 +0530

    Initial Setup: Installed libraries and dataset
    Signed-off-by: gurjarpravin4 <gurjarpravin4@gmail.com>

---
+++ main.py
@@ -0,0 +1,10 @@
+# Imports
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+from torch import nn
+from torchvision import transforms
+
+# Set up paths of train and test data
+train_dir = "data/train"
+test_dir = "data/test"
