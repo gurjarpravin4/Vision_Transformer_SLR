fd555df85140a89d9e946eaa4f45dcef9c9d3049
gurjarpravin4 <gurjarpravin4@gmail.com>
2023-10-27 20:42:02 +0530

    Vision Transformer Model Ready
    Signed-off-by: gurjarpravin4 <gurjarpravin4@gmail.com>

--- main.py
+++ main.py
@@ -170,7 +170,6 @@ if __name__ == '__main__':
     print(f"Patch and position embedding shape: {patch_and_position_embedding.shape}")
     print(patch_embedding_class_token)
 
-
     # Create Multi headed attention block
     class MultiHeadedSelfAttentionBlock(nn.Module):
         """
@@ -281,4 +280,99 @@ if __name__ == '__main__':
 
             # create residual connection for msa block (add input to the output)
             x = self.mlp_block(x) + x
+            return x
+
+
+    # Building a Vision Transformer
+    class ViT(nn.Module):
+        """
+        Creates a ViT architecture with ViT-Base hyperparamters by default
+        """
+
+        # initialize class parameters from table 1 & 3
+        def __init__(
+                self,
+                img_size: int = 224,
+                in_channels: int = 3,
+                patch_size: int = 16,
+                num_transformer_layers: int = 12,
+                embedding_dim: int = 768,
+                mlp_size: int = 3072,
+                num_heads: int = 12,
+                attn_dropout: float = 0,
+                mlp_dropout: float = 0.1,
+                embedding_dropout: float = 0.1,
+                num_classes: int = 1000
+        ):
+            super().__init__()
+
+            # Make sure image size is divisioble by patch size
+            assert img_size % patch_size == 0, (f"input must be divisible by patch size, "
+                                                f"image shape: {img_size}, patch size: {patch_size}")
+
+            # Calculate the number of patches = h*w/patch_size^2
+            self.num_patches = (img_size * img_size) // patch_size ** 2
+
+            # Create learnable class embedding (needs to go at front of sequence patch embeddings)
+            self.class_embedding = nn.Parameter(
+                data=torch.randn(1, 1, embedding_dim),
+                requires_grad=True
+            )
+
+            # Create learnable position embedding
+            self.position_embedding = nn.Parameter(
+                data=torch.randn(1, self.num_patches + 1, embedding_dim),
+                requires_grad=True
+            )
+
+            # create embedding dropout value
+            self.embedding_dropout = nn.Dropout(p=embedding_dropout)
+
+            # Create patch embedding layer
+            self.patch_embedding = PatchEmbedding(
+                in_channels=in_channels,
+                patch_size=patch_size,
+                embedding_dim=embedding_dim
+            )
+
+            # Create transformer encoder blocks
+            # We can stack them using nn.Sequential() NOTE: * means all
+            self.transformer_encoder = nn.Sequential(*[TransformerEncoderBlock(
+                embedding_dim=embedding_dim,
+                num_heads=num_heads,
+                mlp_size=mlp_size,
+                mlp_dropout=mlp_dropout) for i in range(num_transformer_layers)])
+
+            # Create classifier head
+            self.classifier = nn.Sequential(
+                nn.LayerNorm(normalized_shape=embedding_dim),
+                nn.Linear(in_features=embedding_dim, out_features=num_classes)
+            )
+
+        # Create a forward method
+        def forward(self, x):
+            # get batch_size
+            batch_size = x.shape[0]
+
+            # Create class token embedding and expand it to match batch size
+            class_token = self.class_embedding.expand(batch_size, -1, 1)
+
+            # Create patch embedding
+            x = self.patch_embedding(x)
+
+            # Concat class embedding and patch embedding
+            x = torch.cat((class_token, x), 1)
+
+            # Add Position embedding to patch embedding
+            x = self.position_embedding + x
+
+            # Run embedding dropout
+            x = self.embedding_dropout(x)
+
+            # Pass patch, position and class embedding through encoder layers
+            x = self.transformer_encoder(x)
+
+            # Put 0 index logit through classifier
+            x = self.classifier(x[:, 0])
+
             return x
